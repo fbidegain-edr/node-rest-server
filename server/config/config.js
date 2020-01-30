e8726203c7e981c81c3d75b53bbbe2fb4b32cdd7
Facundo Bidegain <fbidegain@edrans.com>
2020-01-30 10:21:28 -0300

    Descomentando config.js

--- server/config/config.js
+++ server/config/config.js
@@ -16,12 +16,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //------- DB -------------------------------------------
 let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe'
-// } else {
-urlDB = 'mongodb+srv://fbidegain:vU0UoHvt8giUC8sA@cluster0-mpuau.mongodb.net/cafe'
-    // }
-    //------------------------------------------------------
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://fbidegain:vU0UoHvt8giUC8sA@cluster0-mpuau.mongodb.net/cafe'
+}
+//------------------------------------------------------
 
 process.env.URLDB = urlDB;
 
